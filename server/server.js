e2b7bdfeb5f5807ff5eed55ca82f161df832db79
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-10-29 17:45:16 -0700

    Bug fixes, api requests working.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const db = require('./db.js');
+const db = require('./db/db.js');
 
-const app = require('express');
+const app = express();
 
 app.get('/restaurants/:restaurantid/reviewsummary', (req, res) => {
   db.getBasicInfo(req.params.restaurantid, (err, data) => {
@@ -11,7 +11,7 @@ app.get('/restaurants/:restaurantid/reviewsummary', (req, res) => {
 });
 
 app.get('/restaurants/:restaurantid/reviews', (req, res) => {
-  db.getReviews(req.params.restaurantId, (err, data) => {
+  db.getReviews(req.params.restaurantid, (err, data) => {
     if (err) { return res.sendStatus(500); }
     res.status(200).json(data);
   });

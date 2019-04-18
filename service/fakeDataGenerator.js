68bbef063a8596e931521ac1b82b8d1dae05b46f
Luis Santana <lsantana30@protonmail.com>
2019-04-18 16:22:51 -0700

    Modified functions to create 2 million users and restaurants as well as 10+ million reviews. Streams to csv files.

--- service/fakeDataGenerator.js
+++ service/fakeDataGenerator.js
@@ -1,12 +1,9 @@
-const faker = require('faker');
-const fs = require('fs');
-const moment = require('moment');
+const faker = require("faker");
+const fs = require("fs");
 
 faker.seed(105);
 
-const fakeRestaurants = [];
-
-for (let i = 1; i <= 100; i += 1) {
+const createFakeRestaurant = i => {
   const fakeRestaurant = { id: i };
   const fakeWords = [];
   const length = faker.random.number() % 3;
@@ -15,31 +12,27 @@ for (let i = 1; i <= 100; i += 1) {
     fakeWords.push(faker.random.word());
   }
 
-  fakeRestaurant.name = fakeWords.join(' ');
+  fakeRestaurant.name = fakeWords.join(" ");
+  return fakeRestaurant;
+};
 
-  fakeRestaurants.push(fakeRestaurant);
-}
-
-const fakeUsers = [];
-
-for (let i = 1; i <= 100; i += 1) {
+const fakeUserGenerator = i => {
   const fakeUser = { id: i };
   fakeUser.name = faker.name.findName();
   fakeUser.numReviews = faker.random.number(50) + 10;
   fakeUser.location = faker.address.city();
-  fakeUsers.push(fakeUser);
-}
-
+  return fakeUser;
+};
 
 const fakeReviewTextGenerator = () => {
   const reviewText = [];
-  const paragraphs = faker.random.number(4);
+  const paragraphs = faker.random.number(2);
   for (let p = 0; p <= paragraphs; p += 1) {
-    const sentences = faker.random.number(3) + 2;
+    const sentences = faker.random.number(2) + 1;
     const paragraph = faker.lorem.paragraph(sentences);
     reviewText.push(paragraph);
   }
-  return reviewText.join('\n\n');
+  return reviewText.join("\n\n");
 };
 
 const ratingGenerator = () => {
@@ -49,34 +42,29 @@ const ratingGenerator = () => {
   return rating === 6 ? 5 : rating;
 };
 
-
 const fakeReviewGenerator = restaurantId => ({
   restaurantId,
-  userId: faker.random.number(99) + 1,
+  userId: faker.random.number(2000000) + 1,
   overallRating: ratingGenerator(),
   foodRating: ratingGenerator(),
   serviceRating: ratingGenerator(),
   ambienceRating: ratingGenerator(),
   valueRating: ratingGenerator(),
   wyr: faker.random.number(4) !== 4 ? 1 : 0,
-  date: (new Date(faker.date.recent(400))).toISOString(),
+  date: new Date(faker.date.recent(400)).toISOString(),
   partySize: faker.random.number(6) + 2,
   text: fakeReviewTextGenerator(),
   timesReported: 0,
-  timesMarkedHelpful: 0,
+  timesMarkedHelpful: 0
 });
-
-
-const fakeReviews = [];
 
 const newAvg = (newVal, avg, newNumOfThings) => {
   const oldTotal = avg * (newNumOfThings - 1);
   return (oldTotal + newVal) / newNumOfThings;
 };
 
-for (let i = 0; i < fakeRestaurants.length; i += 1) {
-  const rest = fakeRestaurants[i];
-  const numReviews = faker.random.number(14) + 3;
+const addReviews = (rest, fakeReviews) => {
+  const numReviews = faker.random.number(7) + 3;
 
   for (let idx = 0; idx < numReviews; idx += 1) {
     const review = fakeReviewGenerator(rest.id);
@@ -86,7 +74,7 @@ for (let i = 0; i < fakeRestaurants.length; i += 1) {
       serviceRating,
       ambienceRating,
       valueRating,
-      wyr,
+      wyr
     } = review;
     if (!rest.numReviews) {
       Object.assign(rest, {
@@ -96,77 +84,142 @@ for (let i = 0; i < fakeRestaurants.length; i += 1) {
         serviceRating,
         ambienceRating,
         valueRating,
-        wyr: wyr ? 100 : 0,
+        wyr: wyr ? 100 : 0
       });
     } else {
       rest.numReviews += 1;
-      rest.overallRating = newAvg(overallRating, rest.overallRating, rest.numReviews);
+      rest.overallRating = newAvg(
+        overallRating,
+        rest.overallRating,
+        rest.numReviews
+      );
       rest.foodRating = newAvg(foodRating, rest.foodRating, rest.numReviews);
-      rest.serviceRating = newAvg(serviceRating, rest.serviceRating, rest.numReviews);
-      rest.ambienceRating = newAvg(ambienceRating, rest.ambienceRating, rest.numReviews);
+      rest.serviceRating = newAvg(
+        serviceRating,
+        rest.serviceRating,
+        rest.numReviews
+      );
+      rest.ambienceRating = newAvg(
+        ambienceRating,
+        rest.ambienceRating,
+        rest.numReviews
+      );
       rest.valueRating = newAvg(valueRating, rest.valueRating, rest.numReviews);
       rest.wyr = newAvg(wyr ? 100 : 0, rest.wyr, rest.numReviews);
     }
     fakeReviews.push(review);
   }
-}
+};
 
-const toCSV = (arrayOfObjects, arrayOfColNames, isIdprovided = true) => {
-  const result = [];
-  result[0] = arrayOfColNames.slice();
-  if (!isIdprovided) {
-    result[0].unshift('id');
-  }
-  result[0] = result[0].join(',');
-  arrayOfObjects.forEach((obj, idx) => {
-    const row = [];
-    if (!isIdprovided) {
-      row[0] = idx + 1;
-    }
-    arrayOfColNames.forEach((colName) => {
+const toCSV = (obj, arrayOfColNames, isIdprovided = true) => {
+  const row = [];
+  arrayOfColNames.forEach((colName, i) => {
+    if (i === 0) {
+      row.push(`\n"${obj[colName]}"`);
+    } else {
       row.push(`"${obj[colName]}"`);
-    });
-    result.push(row.join(','));
+    }
   });
+  return row.join(",");
+};
 
-  return result.join('\n');
+const reviewsToCSV = (arrayOfObjects, arrayOfColNames) => {
+  const result = [];
+  arrayOfObjects.forEach((obj, i) => {
+    const row = [];
+    row[0] = "\n" + ++reviewCount;
+    arrayOfColNames.forEach((colName, j) => {
+      if (i === 0 && j === 0) {
+        row.push(`"${obj[colName]}"`);
+      } else {
+        row.push(`"${obj[colName]}"`);
+      }
+    });
+    result.push(row.join(","));
+  });
+  return result.join("");
 };
 
 const restaurantCols = [
-  'id',
-  'name',
-  'overallRating',
-  'foodRating',
-  'serviceRating',
-  'ambienceRating',
-  'valueRating',
-  'wyr',
-  'numReviews',
+  "id",
+  "name",
+  "overallRating",
+  "foodRating",
+  "serviceRating",
+  "ambienceRating",
+  "valueRating",
+  "wyr",
+  "numReviews"
 ];
 
-const userCols = [
-  'id',
-  'name',
-  'numReviews',
-  'location',
-];
+const userCols = ["id", "name", "numReviews", "location"];
 
 const reviewCols = [
-  'userId',
-  'restaurantId',
-  'overallRating',
-  'foodRating',
-  'serviceRating',
-  'ambienceRating',
-  'valueRating',
-  'wyr',
-  'text',
-  'date',
-  'partySize',
-  'timesReported',
-  'timesMarkedHelpful',
+  "userId",
+  "restaurantId",
+  "overallRating",
+  "foodRating",
+  "serviceRating",
+  "ambienceRating",
+  "valueRating",
+  "wyr",
+  "text",
+  "date",
+  "partySize",
+  "timesReported",
+  "timesMarkedHelpful"
 ];
 
-fs.writeFileSync('./fakeRestaurants.csv', toCSV(fakeRestaurants, restaurantCols));
-fs.writeFileSync('./fakeUsers.csv', toCSV(fakeUsers, userCols));
-fs.writeFileSync('./fakeReviews.csv', toCSV(fakeReviews, reviewCols, false));
+let restStream = fs.createWriteStream("./fakeRestaurants.csv");
+let reviewStream = fs.createWriteStream("./fakeReviews.csv");
+let userStream = fs.createWriteStream("./fakeUsers.csv");
+
+const LIMIT = 2000000;
+let i = 1;
+let reviewCount = 0;
+restStream.write(restaurantCols.slice().join(","));
+let appendedRevCols = reviewCols.slice();
+appendedRevCols.unshift("id");
+reviewStream.write(appendedRevCols.join(","));
+userStream.write(userCols.slice().join(","));
+
+function writing(callback) {
+  let restOK = true,
+    userOK = true,
+    reviewOK = true;
+  do {
+    let fakeReviews = [];
+    let rest = createFakeRestaurant(i);
+    addReviews(rest, fakeReviews);
+    let user = fakeUserGenerator(i);
+    i++;
+    console.log(i);
+    if (i % 10000 === 0) {
+      console.clear();
+    }
+    if (i === LIMIT) {
+      restStream.write(toCSV(rest, restaurantCols), "utf8", callback);
+      userStream.write(toCSV(user, userCols), "utf8", callback);
+      reviewStream.write(
+        reviewsToCSV(fakeReviews, reviewCols),
+        "utf8",
+        callback
+      );
+    } else {
+      restOK = restStream.write(toCSV(rest, restaurantCols));
+      userOK = userStream.write(toCSV(user, userCols));
+      reviewOK = reviewStream.write(reviewsToCSV(fakeReviews, reviewCols));
+    }
+  } while (i <= LIMIT && restOK && reviewOK && userOK);
+  if (i < LIMIT) {
+    if (!reviewOK) {
+      reviewStream.once("drain", writing);
+    } else if (!restOK) {
+      restStream.once("drain", writing);
+    } else {
+      userStream.once("drain", writing);
+    }
+  }
+}
+writing();
+

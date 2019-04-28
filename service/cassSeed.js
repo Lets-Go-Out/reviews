100212db595d5fad26f51c36be85c29885574d9b
Luis Santana <lsantana30@protonmail.com>
2019-04-27 17:34:33 -0700

    Refactored seeding scripts.

--- service/cassSeed.js
+++ service/cassSeed.js
@@ -1,18 +1,39 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: "datacenter1", keyspace: 'reviews' });
+const client = new cassandra.Client({ contactPoints: ['54.183.212.64', '13.56.194.185'], localDataCenter: "us-west", keyspace: 'reviews' });
 
 client.connect((err, result) => {
-
+    console.log(err, result);
 });
 
+let args = process.argv.slice(2);
 const createUsersQuery = 'CREATE TABLE IF NOT EXISTS reviews.users ( id int PRIMARY KEY, name text, numReviews int, location text);';
-const createReviewTypeQuery = 'CREATE TYPE reviews.review ( reviewId int, userId int, overallrating int, foodrating int, servicerating int, ambiencerating int, valuerating int, wyr int, date timestamp, partysize int, text text, timesreported int, timesmarkedhelpful int);'
-const createRestaurantsQuery = 'CREATE TABLE IF NOT EXISTS reviews.restaurants (id int PRIMARY KEY, name text, overallrating decimal, foodrating decimal, servicerating decimal, ambiencerating decimal, valuerating decimal, wyr decimal, numreviews int, reviews list<frozen<review>>)'
+const createReviewTypeQuery = 'CREATE TYPE reviews.review ( reviewId int, userId int, overallrating int, foodrating int, servicerating int, ambiencerating int, valuerating int, wyr int, date timestamp, partysize int, text text, timesreported int, timesmarkedhelpful int);';
+const createRestaurantsQuery = 'CREATE TABLE IF NOT EXISTS reviews.restaurants (id int PRIMARY KEY, name text, overallrating decimal, foodrating decimal, servicerating decimal, ambiencerating decimal, valuerating decimal, wyr decimal, numreviews int, reviews list<frozen<review>>);';
 const queries = [{ query: createUsersQuery }, { query: createReviewTypeQuery }, { query: createRestaurantsQuery }];
 
-client.bacth(queries)
-    .then(result => console.log(result));
+const queryOptions = {
+    createKeyspace: client.execute('create keyspace reviews with replication = {\'class\': \'NetworkTopologyStrategy\', \'us-west\': 3};').then(result => console.log(result)),
+    createUsers: client.execute(createUsersQuery).then(result => console.log(result)),
+    createRestaurants: client.execute(createRestaurantsQuery).then(result => console.log(result)),
+    createReviewtype: client.execute(createReviewTypeQuery).then(result => console.log(result)),
+    createAll: client.batch(queries).then(result => console.log(result)),
+    copyQueryUsers: client.execute("COPY  '/mnt/c/Users/lsant/Documents/Programming/JavaScript/SEI/reviews/service/fakeNoSQLUsers.csv' WITH HEADER = TRUE AND DELIMITER = '#' AND QUOTE = '''';")
+        .then(result => console.log(result)),
+    copyQueryRestaurants: client.execute("COPY reviews.restaurants (id, name, overallrating, foodrating, servicerating, ambiencerating, valuerating, wyr, numreviews, reviews) FROM '/mnt/c/Users/lsant/Documents/Programming/JavaScript/SEI/reviews/service/fakeNoSQLRestaurants.csv' WITH HEADER = TRUE AND DELIMITER = '#' AND QUOTE = '''';")
+        .then(result => console.log(result)),
+    completeSelectQuery: ((restId) => {
+        const selectRestQuery = "select * from  reviews.restaurants where id = ?"
+        const selectUserQuery = "select * from reviews.users where id = ?"
+        client.execute(selectRestQuery, [restId], { prepare: true, traceQuery: true })
+            .then(result => {
+                let reviews = JSON.parse(result.rows[0].reviews);
+                reviews.forEach(review => {
+                    client.execute(selectUserQuery, [review.userid], { prepare: true, traceQuery: true })
+                        .then(result => console.log(result))
+                        .catch(err => console.log(err));
+                });
+            });
+    })(args[1])
+}
 
-const copyQuery = "COPY reviews.users (id, name, numreviews, location) FROM '/mnt/c/Users/lsant/Documents/Programming/JavaScript/SEI/reviews/service/fakeNoSQLUsers.csv' WITH HEADER = TRUE AND DELIMITER = '#' ;"
-client.execute(copyQuery)
-    .then(result => console.log(result));
+queryOptions[args[0]];
--- service/pgSeed.js
+++ service/pgSeed.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const pass = require("./pass.js");
+const fs = require('fs');
 
 const pool = new Pool({
   user: "postgres",
@@ -9,41 +10,60 @@ const pool = new Pool({
   port: 5433
 });
 
-pool.query('create table if not exists restaurants (id integer Primary Key, name varchar(200), "overallRating" double precision, "foodRating" double precision, "serviceRating" double precision, "ambienceRating" double precision, "valueRating" double precision, wyr double precision, "numReviews" integer)', (err, res) => {
-  console.log(err, res);
-  // pool.end();
-});
 
-pool.query('create table if not exists reviews (id integer Primary Key, "userId" integer, "restaurantId" integer, "overallRating" integer, "foodRating" integer, "serviceRating" integer, "ambienceRating" integer, "valueRating" integer, wyr integer, text text, date varchar(50), "partySize" integer, "timesReported" integer, "timesMarkedHelpful" integer)', (err, res) => {
-  console.log(err, res);
-  // pool.end();
-});
+// Example usage
+// node pgSeed createRestaurants
+// node pgSeed copyRestaurants
+// node pgSeed queryMultiple 231240 232240
 
-pool.query('create table if not exists users (id integer Primary Key, name varchar(50), "numReviews" integer, location varchar(50))', (err, res) => {
-  console.log(err, res);
-  // pool.end();
-});
 
-pool.query(
-  'copy public.reviews (id, "userId", "restaurantId", "overallRating", "foodRating", "serviceRating", "ambienceRating", "valueRating", wyr, text, date, "partySize", "timesReported", "timesMarkedHelpful") FROM \'C:/Windows/Temp/PostgreSQL/fakeReviews2.csv\' DELIMITER \',\' CSV HEADER QUOTE \'"\' ESCAPE \'\'\'\';',
-  (err, res) => {
+let args = process.argv.slice(2);
+
+const queryOptions = {
+  createRestaurants: pool.query('create table if not exists restaurants (id integer Primary Key, name varchar(200), "overallRating" double precision, "foodRating" double precision, "serviceRating" double precision, "ambienceRating" double precision, "valueRating" double precision, wyr double precision, "numReviews" integer, "reviewIds" integer ARRAY);', (err, res) => {
     console.log(err, res);
-    // pool.end();
-  }
-);
-
-pool.query(
-  'copy restaurants (id, name, "overallRating", "foodRating", "serviceRating", "ambienceRating", "valueRating", wyr, "numReviews") FROM \'C:/Windows/Temp/PostgreSQL/fakeRestaurants2.csv\' DELIMITER \',\' CSV HEADER QUOTE \'"\' ESCAPE \'\'\'\';',
-  (err, res) => {
+  }),
+  createReviews: pool.query('create table if not exists reviews (id integer, "userId" integer, "restaurantId" integer, "overallRating" integer, "foodRating" integer, "serviceRating" integer, "ambienceRating" integer, "valueRating" integer, wyr integer, text text, date varchar(50), "partySize" integer, "timesReported" integer, "timesMarkedHelpful" integer, PRIMARY KEY(id, "restaurantId"))', (err, res) => {
     console.log(err, res);
-    // pool.end();
-  }
-);
-
-pool.query(
-  "copy users (id, name, \"numReviews\", location) FROM 'C:/Windows/Temp/PostgreSQL/fakeUsers.csv' DELIMITER ',' CSV HEADER QUOTE '\"' ESCAPE '''';",
-  (err, res) => {
+  }),
+  createUsers: pool.query('create table if not exists users (id integer Primary Key, name varchar(50), "numReviews" integer, location varchar(50))', (err, res) => {
     console.log(err, res);
-    pool.end();
-  }
-);
+  }),
+  copyRestaurants: pool.query(
+    'copy restaurants (id, name, "overallRating", "foodRating", "serviceRating", "ambienceRating", "valueRating", wyr, "numReviews", "reviewIds") FROM \'C:/Windows/Temp/PostgreSQL/restaurants.csv\' DELIMITER \',\' CSV HEADER QUOTE \'"\' ESCAPE \'\'\'\';',
+    (err, res) => {
+      console.log(err, res);
+    }
+  ),
+  copyReviews: pool.query(
+    'copy reviews (id, "userId", "restaurantId", "overallRating", "foodRating", "serviceRating", "ambienceRating", "valueRating", wyr, text, date, "partySize", "timesReported", "timesMarkedHelpful") FROM \'C:/Windows/Temp/PostgreSQL/reviews.csv\' DELIMITER \',\' CSV HEADER QUOTE \'"\' ESCAPE \'\'\'\';',
+    (err, res) => {
+      console.log(err, res);
+    }
+  ),
+  copyUsers: pool.query(
+    "copy users (id, name, \"numReviews\", location) FROM 'C:/Windows/Temp/PostgreSQL/users.csv' DELIMITER ',' CSV HEADER QUOTE '\"' ESCAPE '''';",
+    (err, res) => {
+      console.log(err, res);
+    }
+  ),
+  queryMultiple: ((start, stop) => {
+    let queryStream = fs.createWriteStream("./queryTimes.csv");
+    let queryOK = true, row = "", i = start;
+
+    const queries = () => {
+      for (i; i <= stop; i++) {
+        pool.query('explain analyze select * from users join (select * from reviews, restaurants where reviews.id = any( restaurants."reviewIds"::int[]) and restaurants.id = $1) sub on users.id = sub."userId"; ', [i], (err, res) => {
+          if (err) {
+            pool.end();
+          }
+          row = `\n${res.rows[8]['QUERY PLAN'].split(' ')[2]}, ${res.rows[9]['QUERY PLAN'].split(' ')[2]}, ${+res.rows[8]['QUERY PLAN'].split(' ')[2] + +res.rows[9]['QUERY PLAN'].split(' ')[2]}`
+          queryOK = queryStream.write(row);
+        })
+      }
+      if (!queryOK) queryStream.once('drain', queries);
+    }
+  })(args[1], args[2])
+}
+
+queryOptions[args[0]];
